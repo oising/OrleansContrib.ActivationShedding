607ede535a1a1f03aafbb4f867be7ca69900012b
Oisin G <oising@gmail.com>
2023-07-09 17:14:12 -0400

    updated for orleans 7.2.0; use new migration api

---
+++ IGrainMigrationEligibilityCheck.cs
@@ -0,0 +1,18 @@
+﻿using Orleans;
+
+namespace OrleansContrib.ActivationShedding
+{
+    /// <summary>
+    /// Defines an implementation that will decide which grains are eligible to be migrated on a silo that is shedding activations.
+    /// </summary>
+    public interface IGrainMigrationEligibilityCheck
+    {
+        /// <summary>
+        /// Called to decide if a grain is eligible to be migrated on a silo that is shedding activations.
+        /// Typically one might decide to filter on the grain's Type namespace, e.g. Foobar.Grains.*
+        /// </summary>
+        /// <param name="grain">The contextual grain to check</param>
+        /// <returns>True if we can try to migrate the grain, false if not.</returns>
+        bool ShouldBeMigrated(IGrainBase grain);
+    }
+}
